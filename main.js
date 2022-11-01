2227fe20c44c4c58141bc6393327638a8c6f10d8
MrRabotnik <sargis2014@gmail.com>
2022-11-02 03:37:46 +0400

    Update

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//Basic Actions 
 let canvas = document.getElementById("space")
 let ctx = canvas.getContext("2d")
 
@@ -6,37 +7,60 @@ let h = window.innerHeight
 canvas.width = w
 canvas.height = h
 
+// Starting the code
 ctx.strokeStyle = 'white'
 ctx.fillStyle = 'white'
 
-let radius = 30
-let growRadius = null
-let mouseX, mouseY;
+let stars = []
 
-function drawCircleAroundCursor(mouseX, mouseY) {
+let middlePointX = w / 2
+let middlePointY = h / 2
+
+let animation;
+    
+function drawStar(x, y, radius) {
     ctx.beginPath();
-    ctx.clearRect(0, 0, w, h);
-    ctx.arc(mouseX, mouseY, radius, 0, 2 * Math.PI);
+    ctx.arc(x, y, radius, 0, 2 * Math.PI);
     ctx.closePath();
     ctx.fill();
 }
 
+function drawSpace() {
+    ctx.clearRect(0, 0, w, h)
+    for (let star of stars) {
+        drawStar(star?.x, star?.y, star?.radius)
+    }
+}
+
+for (let i = 0; i < 100; i++) {
+    const x = Math.floor((Math.random() * (w - 50)) + 30)
+    const y = Math.floor((Math.random() * (h - 50)) + 30)
+    const radius = Math.floor((Math.random() * 15) + 5)
+    stars.push({ x, y, radius })
+}
+
+drawSpace()
+
 document.addEventListener('mousemove', (e) => {
+    clearInterval(animation)
     mouseX = e.clientX
     mouseY = e.clientY
-    drawCircleAroundCursor(mouseX, mouseY)
+    const interval = Math.abs(mouseX - middlePointX) + Math.abs(mouseY - middlePointY) > 600 ? 600 : Math.abs(mouseX - middlePointX) + Math.abs(mouseY - middlePointY)
+    animation = setInterval(() => {
+        stars = stars.map(star => {
+            if (mouseX > middlePointX) star.x += (mouseX - middlePointX) / 5
+            if (mouseY > middlePointY) star.y += (mouseY - middlePointY) / 10
+            if (mouseX < middlePointX) star.x -= (middlePointX - mouseX) / 5
+            if (mouseY < middlePointY) star.y -= (middlePointY - mouseY) / 10
+                
+            if (star.x > w) star.x = 0
+            if (star.y > h) star.y = 0
+            if (star.x < 0) star.x = w
+            if (star.y < 0) star.y = h
+            return star
+        })
+        drawSpace()
+    }, 600 - interval);
 })
 
-document.addEventListener('mousedown', (e) => {
-    growRadius = setInterval(() => {
-        radius += 1
-        drawCircleAroundCursor(mouseX, mouseY)
-    }, 0)
-})
-
-document.addEventListener('mouseup', (e) => {
-    clearInterval(growRadius)
-    radius = 30
-    drawCircleAroundCursor(mouseX, mouseY)
-})
 
